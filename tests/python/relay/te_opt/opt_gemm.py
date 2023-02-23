32fde8997fc9cc80fb525ded7530911ee879bfce
zhangshuoming <zhangshuoming17@mails.ucas.ac.cn>
2023-02-23 15:24:23 +0800

    [te][ansor]learned basic ansor schedule search

--- tests/python/relay/te_opt/opt_gemm.py
+++ tests/python/relay/te_opt/opt_gemm.py
@@ -13,27 +13,9 @@ def matmul_te(A,B):
     C = te.compute((M, N), lambda m, n: te.sum(A[m, k] * B[k, n], axis=k), name="C")
     return C
 
-vM = te.size_var("M")
-vK = te.size_var("K")
-vN = te.size_var("N")
-pA = te.placeholder((vM, vK), name="A")
-pB = te.placeholder((vK, vN), name="B")
 
-M = 256
-K = 256
-N = 256
-dtype="float32"
 
-# Random generated tensor for testing
-a = tvm.nd.array(np.ones(shape=(M, K)).astype(dtype))
-b = tvm.nd.array(np.ones(shape=(K, N)).astype(dtype))
-c = tvm.nd.array(np.zeros((M, N), dtype=dtype))
-answer = np.matmul(a.numpy(), b.numpy())
-pC = matmul_te(A=pA, B=pB)
-# Default schedule
-s: te.schedule.Schedule = te.create_schedule(pC.op)
-
-def cpu_gemm(pA,pB,pC):
+def cpu_gemm(pA,pB,pC,s:te.schedule.Schedule):
 
     # print("-----tir code-----")
     # print(tvm.lower(s, [pA, pB, pC], simple_mode=True))
@@ -46,65 +28,98 @@ def cpu_gemm(pA,pB,pC):
     # print("-----CPU code-----")
     # print(cpu_func.get_source())
 
-cpu_gemm(pA,pB,pC)
 
 
-def gpu_gemm(pA,pB,pC):
+
+def gpu_gemm_naive(pA,pB,pC,s:te.schedule.Schedule):
     # C[m,n] += A[m,k]*B[k,n]
-
-    block_size = 16
-    tx, ty, tk = 16, 16, 16
     ctx = tvm.cuda()
-    def split(stage, axis, factors):
-        axes=[]
-        for f in reversed(factors):
-            axis, x = stage.split(axis, f)
-            axes.append(x)
-        return list(reversed(axes+[axis]))
+    block_size = 4
 
-    def bind_thread(stage, axes, tags):
-        for axis, tag in zip(axes, tags):
-            stage.bind(axis, te.thread_axis(tag))
+    # schedule
+    y, x = s[pC].op.axis
+    k = s[pC].op.reduce_axis[0]
+    
+    yo, yi = s[pC].split(y, factor=block_size)
+    xo, xi = s[pC].split(x, factor=block_size)
 
-    def optimize_read_cache(shared, local):
-        s[shared].compute_at(s[C_local], ko)
-        s[local].compute_at(s[C_local], ki)
-        y,x = s[shared].op.axis
-        yo, yi = s[shared].split(y, nparts=block_size)
-        xo, xi = s[shared].split(x, nparts=block_size)
-        s[shared].reorder(yo, xo, yi, xi)
-        bind_thread(s[shared], (yo,xo), ("threadIdx.y", "threadIdx.x"))
+    s[pC].bind(yo, te.thread_axis("blockIdx.y"))
+    s[pC].bind(xo, te.thread_axis("blockIdx.x"))
+    s[pC].bind(yi, te.thread_axis("threadIdx.y"))
+    s[pC].bind(xi, te.thread_axis("threadIdx.x"))
+    # s[pC].reorder(yo, xo, k, yi, xi)
 
-    A_shared = s.cache_read(pA, "shared", [pC])
-    A_local  = s.cache_read(A_shared, "local", [pC])
-    B_shared = s.cache_read(pB, "shared", [pC])
-    B_local  = s.cache_read(B_shared, "local", [pC])
-    C_local = s.cache_write(pC, "local")
-    # Split each axis into block axis, thread axis, and inner axis
-    x, y = s[pC].op.axis
-    xb,xo,xi = split(s[pC],x,(block_size, tx))
-    yb,yo,yi = split(s[pC],y,(block_size, ty))
-    s[pC].reorder(xb, yb, xo, yo, xi, yi)
-    # we bind yb to blockIdx.x instead of blockIdx.y
-    bind_thread(s[pC], [yb, xb, yo, xo],("blockIdx.x", "blockIdx.y", "threadIdx.x", "threadIdx.y"))
-    # schedule C_local
-    s[C_local].compute_at(s[pC], yo)
-    yi,xi = s[C_local].op.axis
-    k, = s[C_local].op.reduce_axis
-    ko,ki = s[C_local].split(k, tk)
-    s[C_local].reorder(ko, ki, yi, xi)
-
-    optimize_read_cache(A_shared, A_local)
-    optimize_read_cache(B_shared, B_local)
-    print("-"*10,"tir code","-"*10)
-    print(tvm.lower(s, [pA, pB, pC], simple_mode=True))
-
-
-
+    # print("-"*10,"tir code","-"*10)
+    # print(tvm.lower(s, [pA, pB, pC], simple_mode=True))
     cuda_func = tvm.build(s, [pA, pB, pC], target="cuda", name="matmul")
     dev_module = cuda_func.imported_modules[0]
-    print("-"*10,"GPU code","-"*10)
-    print(dev_module.get_source())
+    # print("-"*10,"GPU code","-"*10)
+    # print(dev_module.get_source())
+    dev = tvm.cuda(0)
+    a2 = tvm.nd.array(np.ones(shape=(M, K)).astype(dtype),device=dev)
+    b2 = tvm.nd.array(np.ones(shape=(K, N)).astype(dtype),device=dev)
+    c2 = tvm.nd.array(np.zeros((M, N), dtype=dtype),device=dev)
+    cuda_func(a2,b2,c2)
+    # print(c2)
+    tvm.testing.assert_allclose(c2.numpy(), answer, rtol=1e-5)
+    evaluator = cuda_func.time_evaluator(cuda_func.entry_name,dev=tvm.cuda(0), number=1)
+    print("GPU naive: %f" % evaluator(a2, b2, c2).mean)
+
+
+    
+def gpu_gemm(A,B,C,s:te.schedule.Schedule):
+    # C[m,n] += A[m,k]*B[k,n]
+    C_i, C_j, C_k = tuple(C.op.axis) + tuple(C.op.reduce_axis)
+    C_local, = s.cache_write([C], "local")
+    C_local_i_c, C_local_j_c, C_local_k = tuple(C_local.op.axis) + tuple(C_local.op.reduce_axis)
+    C_local_i_c_o_i, C_local_i_c_i = s[C_local].split(C_local_i_c, factor=4)
+    C_local_i_c_o_o_i, C_local_i_c_o_i = s[C_local].split(C_local_i_c_o_i, factor=4)
+    C_local_i_c_o_o_o_i, C_local_i_c_o_o_i = s[C_local].split(C_local_i_c_o_o_i, factor=1)
+    C_local_i_c_o_o_o_o, C_local_i_c_o_o_o_i = s[C_local].split(C_local_i_c_o_o_o_i, factor=1)
+    C_local_j_c_o_i, C_local_j_c_i = s[C_local].split(C_local_j_c, factor=1)
+    C_local_j_c_o_o_i, C_local_j_c_o_i = s[C_local].split(C_local_j_c_o_i, factor=1)
+    C_local_j_c_o_o_o_i, C_local_j_c_o_o_i = s[C_local].split(C_local_j_c_o_o_i, factor=64)
+    C_local_j_c_o_o_o_o, C_local_j_c_o_o_o_i = s[C_local].split(C_local_j_c_o_o_o_i, factor=4)
+    C_local_k_o_i, C_local_k_i = s[C_local].split(C_local_k, factor=1)
+    C_local_k_o_o, C_local_k_o_i = s[C_local].split(C_local_k_o_i, factor=4)
+    s[C_local].reorder(C_local_i_c_o_o_o_o, C_local_j_c_o_o_o_o, C_local_i_c_o_o_o_i, C_local_j_c_o_o_o_i, C_local_i_c_o_o_i, C_local_j_c_o_o_i, C_local_k_o_o, C_local_k_o_i, C_local_i_c_o_i, C_local_j_c_o_i, C_local_k_i, C_local_i_c_i, C_local_j_c_i)
+    C_i_o_i, C_i_i = s[C].split(C_i, factor=16)
+    C_i_o_o_i, C_i_o_i = s[C].split(C_i_o_i, factor=1)
+    C_i_o_o_o, C_i_o_o_i = s[C].split(C_i_o_o_i, factor=1)
+    C_j_o_i, C_j_i = s[C].split(C_j, factor=1)
+    C_j_o_o_i, C_j_o_i = s[C].split(C_j_o_i, factor=64)
+    C_j_o_o_o, C_j_o_o_i = s[C].split(C_j_o_o_i, factor=4)
+    s[C].reorder(C_i_o_o_o, C_j_o_o_o, C_i_o_o_i, C_j_o_o_i, C_i_o_i, C_j_o_i, C_i_i, C_j_i)
+    s[C_local].compute_at(s[C], C_j_o_i)
+    B_shared = s.cache_read(B, "shared", [C_local])
+    B_shared_ax0, B_shared_ax1 = tuple(B_shared.op.axis)
+    s[B_shared].compute_at(s[C_local], C_local_k_o_o)
+    A_shared = s.cache_read(A, "shared", [C_local])
+    A_shared_ax0, A_shared_ax1 = tuple(A_shared.op.axis)
+    s[A_shared].compute_at(s[C_local], C_local_k_o_o)
+    C_i_o_o_o_j_o_o_o_fused = s[C].fuse(C_i_o_o_o, C_j_o_o_o)
+    s[C].bind(C_i_o_o_o_j_o_o_o_fused, te.thread_axis("blockIdx.x"))
+    C_i_o_o_i_j_o_o_i_fused = s[C].fuse(C_i_o_o_i, C_j_o_o_i)
+    s[C].bind(C_i_o_o_i_j_o_o_i_fused, te.thread_axis("vthread"))
+    C_i_o_i_j_o_i_fused = s[C].fuse(C_i_o_i, C_j_o_i)
+    s[C].bind(C_i_o_i_j_o_i_fused, te.thread_axis("threadIdx.x"))
+    B_shared_ax0_ax1_fused = s[B_shared].fuse(B_shared_ax0, B_shared_ax1)
+    B_shared_ax0_ax1_fused_o, B_shared_ax0_ax1_fused_i = s[B_shared].split(B_shared_ax0_ax1_fused, factor=4)
+    s[B_shared].vectorize(B_shared_ax0_ax1_fused_i)
+    B_shared_ax0_ax1_fused_o_o, B_shared_ax0_ax1_fused_o_i = s[B_shared].split(B_shared_ax0_ax1_fused_o, factor=64)
+    s[B_shared].bind(B_shared_ax0_ax1_fused_o_i, te.thread_axis("threadIdx.x"))
+    A_shared_ax0_ax1_fused = s[A_shared].fuse(A_shared_ax0, A_shared_ax1)
+    A_shared_ax0_ax1_fused_o, A_shared_ax0_ax1_fused_i = s[A_shared].split(A_shared_ax0_ax1_fused, factor=1)
+    s[A_shared].vectorize(A_shared_ax0_ax1_fused_i)
+    A_shared_ax0_ax1_fused_o_o, A_shared_ax0_ax1_fused_o_i = s[A_shared].split(A_shared_ax0_ax1_fused_o, factor=64)
+    s[A_shared].bind(A_shared_ax0_ax1_fused_o_i, te.thread_axis("threadIdx.x"))
+    s[C_local].pragma(C_local_i_c_o_o_o_o, "auto_unroll_max_step", 1024)
+    s[C_local].pragma(C_local_i_c_o_o_o_o, "unroll_explicit", True)
+
+    cuda_func = tvm.build(s, [A, B, C], target="cuda", name="matmul")
+    dev_module = cuda_func.imported_modules[0]
+    # print("-"*10,"GPU code","-"*10)
+    # print(dev_module.get_source())
     dev = tvm.cuda(0)
     a2 = tvm.nd.array(np.ones(shape=(M, K)).astype(dtype),device=dev)
     b2 = tvm.nd.array(np.ones(shape=(K, N)).astype(dtype),device=dev)
@@ -115,4 +130,35 @@ def gpu_gemm(pA,pB,pC):
     evaluator = cuda_func.time_evaluator(cuda_func.entry_name,dev=tvm.cuda(0), number=1)
     print("GPU shared: %f" % evaluator(a2, b2, c2).mean)
 
-gpu_gemm(pA,pB,pC)
+
+
+if __name__ == "__main__":
+    vM = te.size_var("M")
+    vK = te.size_var("K")
+    vN = te.size_var("N")
+    pA = te.placeholder((vM, vK), name="A")
+    pB = te.placeholder((vK, vN), name="B")
+
+    M = 4096
+    K = 4096
+    N = 2048
+    dtype="float32"
+
+    # Random generated tensor for testing
+    a = tvm.nd.array(np.ones(shape=(M, K)).astype(dtype))
+    b = tvm.nd.array(np.ones(shape=(K, N)).astype(dtype))
+    c = tvm.nd.array(np.zeros((M, N), dtype=dtype))
+    answer = np.matmul(a.numpy(), b.numpy())
+    pC = matmul_te(A=pA, B=pB)
+    # Default schedule
+    # s: te.schedule.Schedule = te.create_schedule(pC.op)
+    # cpu_gemm(pA,pB,pC,s)
+    # GPU sharemem schedule
+    # s: te.schedule.Schedule = te.create_schedule(pC.op)
+    # gpu_gemm_shared(pA,pB,pC,s)
+
+    # GPU schedule
+    s: te.schedule.Schedule = te.create_schedule(pC.op)
+    gpu_gemm_naive(pA,pB,pC,s)
+    s: te.schedule.Schedule = te.create_schedule(pC.op)
+    gpu_gemm(A=pA,B=pB,C=pC,s=s)
